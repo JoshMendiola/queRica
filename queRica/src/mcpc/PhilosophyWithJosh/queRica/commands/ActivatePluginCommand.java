655d6523b8fb8bcac145c00921cb42bbadbc241e
guzyw <guzyw@10.5.47.172>
2022-04-20 19:09:31 -0500

    added functionality

--- queRica/src/mcpc/PhilosophyWithJosh/queRica/commands/ActivatePluginCommand.java
+++ queRica/src/mcpc/PhilosophyWithJosh/queRica/commands/ActivatePluginCommand.java
@@ -1,7 +1,35 @@
 package mcpc.PhilosophyWithJosh.queRica.commands;
 
+import java.util.concurrent.Executor;
 
-public class ActivatePluginCommand 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import mcpc.PhilosophyWithJosh.queRica.Main;
+
+public class ActivatePluginCommand implements CommandExecutor
 {
+	private Main plugin;
+	private static boolean commandIsActive = false;
+	public ActivatePluginCommand(Main plugin)
+	{
+		this.plugin = plugin;
+		plugin.getCommand("qrtoggle").setExecutor(this);
+	}
 	
+	public boolean onCommand(CommandSender sender, Command beblockhunter, String label, String[] args)
+	
+	{
+		plugin.getServer().broadcastMessage("plugin toggled");
+		commandIsActive = !commandIsActive;
+		return true;
+	}
+
+	public static boolean commandIsActive() {
+		return commandIsActive;
+	}
+
 }
+	
+
--- queRica/src/mcpc/PhilosophyWithJosh/queRica/listeners/BlockBreakListener.java
+++ queRica/src/mcpc/PhilosophyWithJosh/queRica/listeners/BlockBreakListener.java
@@ -1,17 +1,25 @@
 package mcpc.PhilosophyWithJosh.queRica.listeners;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 import mcpc.PhilosophyWithJosh.queRica.Main;
+import mcpc.PhilosophyWithJosh.queRica.commands.ActivatePluginCommand;
 
 public class BlockBreakListener implements Listener
 {
 	private Main plugin;
-	
+	ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
 	public BlockBreakListener(Main plugin)
 	{
 		this.plugin = plugin;
@@ -22,5 +30,31 @@ public class BlockBreakListener implements Listener
 	public void playerBrokeBlock(BlockBreakEvent bbe)
 	{
 		
+		if(ActivatePluginCommand.commandIsActive())
+			drops.clear();
+			bbe.setCancelled(true);
+			bbe.getPlayer().sendMessage("Plugin Works");
+		
+		
+			Block brokenBlock = bbe.getBlock();
+			Collection<ItemStack> itemList = brokenBlock.getDrops();
+			Location blockLocation = brokenBlock.getLocation();
+			
+			
+			for(ItemStack item: itemList) 
+			{
+				int randomNumber = (int) (Math.random() * 25);
+				Material dropped = item.getType();
+				
+				drops.add(new ItemStack (dropped,randomNumber));
+				//ItemStack desiredDrops = new ItemStack(dropped,randomNumber);
+			}
+			
+			brokenBlock.setType(Material.AIR);
+			
+			for (int i = 0; i < drops.size(); i++) 
+			{
+				brokenBlock.getWorld().dropItemNaturally(blockLocation, drops.get(i));
+			}
 	}
 }
