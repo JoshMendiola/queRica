1242016de04f149155a040be13fd5696e8558514
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-04-21 23:46:48 -0500

    cleaned up the code a bit

--- queRica/src/mcpc/PhilosophyWithJosh/queRica/listeners/BlockBreakListener.java
+++ queRica/src/mcpc/PhilosophyWithJosh/queRica/listeners/BlockBreakListener.java
@@ -20,6 +20,7 @@ public class BlockBreakListener implements Listener
 {
 	private Main plugin;
 	ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
+	
 	public BlockBreakListener(Main plugin)
 	{
 		this.plugin = plugin;
@@ -29,18 +30,19 @@ public class BlockBreakListener implements Listener
 	@EventHandler
 	public void playerBrokeBlock(BlockBreakEvent bbe)
 	{
-		
+		//checks if command is active
 		if(ActivatePluginCommand.commandIsActive())
+			//clears arraylist and cancels event
 			drops.clear();
 			bbe.setCancelled(true);
 			bbe.getPlayer().sendMessage("Plugin Works");
 		
-		
+			//grabs necessary information
 			Block brokenBlock = bbe.getBlock();
 			Collection<ItemStack> itemList = brokenBlock.getDrops();
 			Location blockLocation = brokenBlock.getLocation();
 			
-			
+			//loops through and gets every item that drops
 			for(ItemStack item: itemList) 
 			{
 				int randomNumber = (int) (Math.random() * 25);
@@ -50,8 +52,10 @@ public class BlockBreakListener implements Listener
 				//ItemStack desiredDrops = new ItemStack(dropped,randomNumber);
 			}
 			
+			//sets block to air
 			brokenBlock.setType(Material.AIR);
 			
+			//drops item
 			for (int i = 0; i < drops.size(); i++) 
 			{
 				brokenBlock.getWorld().dropItemNaturally(blockLocation, drops.get(i));
